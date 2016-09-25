1a3e4f530935a29f5482d0899b6d2d9501e5755f
Jazmine Angoma <jazmine.angoma@gmail.com>
2016-09-25 16:43:41 -0500

    validacion ultima de formulario

--- js/app.js
+++ js/app.js
@@ -41,49 +41,91 @@ window.onload = function () {
 			bloque4.classList.add("no"); 
 	});
 
-}
+	/*formulario*/
+		var btn = document.getElementById("boton");
+	var nombre = document.getElementById("input-form1");
+	var correo = document.getElementById("input-form2");
+	var texto = document.getElementById("textarea-form");
 
-window.addEventListener("load", function(){
-	var btn = document.getElementById("boton");
+
 	btn.addEventListener("click", function(event){
 		event.preventDefault();
-// Validar un campo de texto obligatorio //
-		var cuadroDeValidacionNombre = document.createElement("span");
-		var cuadroDeValidacionEmail = document.createElement("span");
-		var cuadroDeValidacionMensaje = document.createElement("span");
-
-		var campoObliInputUno = document.getElementById("input-form1");
-		var campoObliInputDos = document.getElementById("input-form2");
-		var campoObliTexarea = document.getElementById("textarea-form");
-
-			if (campoObliInputUno.value == null || campoObliInputUno.value == 0 || /^\s+$/.test(campoObliInputUno.value) ) {
-			/*	var cuadroNegro = document.getElementsByClassName("input-box")[0];
-				cuadroNegro.appendChild(cuadroDeValidacionNombre);
-				cuadroDeValidacionNombre.innerHTML = "<strong>Completar los campos</strong>";
-				cuadroNegro.classList.add("error");*/
-				alert("Llenar campo nombre de forma correcta");
-			};
-
-			if (campoObliInputDos.value == null || campoObliInputDos.value == 0 || /^\s+$/.test(campoObliInputDos.value) ) {
-				alert("Llenar campo correo de forma correcta");
-			};
-
-			if (campoObliTexarea.value == null || campoObliTexarea.value == 0 || /^\s+$/.test(campoObliTexarea.value) ) {
-				alert("Llenar campo texto de forma correcta");
-			};
-
-			// Validar un campo de texto nombre solo caracteres nombre//
-			if ( /^[a-z][a-z]*/.test(campoObliInputUno.value)==false ) {
-				alert ('Nombre no valido'); 
-			};
-
-			//  Validar una dirección de email//
-
-			if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(campoObliInputDos.value)) ) {
-				alert(" Validar una dirección de email");
-			}
-
-
-
+		validarForm();
 	});
-});
+
+	nombre.addEventListener("focus", function(){
+		var cuadroNegro = document.getElementById("mensaje-error-nombre");
+		cuadroNegro.className = "ocultar";
+	});
+	
+	correo.addEventListener("focus", function(){
+		var cuadroNegro = document.getElementById("mensaje-error-correo");
+		cuadroNegro.className = "ocultar";		
+	});
+
+	
+	texto.addEventListener("focus", function(){
+		var cuadroNegro = document.getElementById("mensaje-error-texto");
+		cuadroNegro.className = "ocultar";		
+	});
+
+	nombre.addEventListener("blur", function(){
+		validarNombre(nombre);
+	});
+
+	correo.addEventListener("blur", function(){
+		validarCorreo(correo);
+	});
+
+	texto.addEventListener("blur", function(){
+		validarTextarea(texto);
+	});		
+
+
+	function validarForm(){
+		validarNombre(nombre);
+		validarCorreo(correo);
+		validarTextarea(texto);
+	}
+};
+
+
+var validarNombre = function(nombre){
+    var cuadroNegro = document.getElementById("mensaje-error-nombre");
+	// Validar un campo de texto obligatorio //
+	if (nombre.value == null || nombre.value == 0 || /^\s+$/.test(nombre.value) ) {
+        cuadroNegro.innerHTML = "Debe ingresar su nombre";
+        cuadroNegro.className = "mostrar";
+	}
+
+	// Validar un campo de texto nombre solo caracteres nombre//
+	else if ( /^[a-z][a-z]*/.test(nombre.value)==false ) {	
+        cuadroNegro.innerHTML = "Debe ingresar su caracteres correctos";  
+        cuadroNegro.className = "mostrar";   
+	};
+};
+
+var validarCorreo = function(correo){
+	var cuadroNegro = document.getElementById("mensaje-error-correo");
+	// Validar un campo de texto obligatorio //
+	if (correo.value == null || correo.value == 0 || /^\s+$/.test(correo.value) ) {
+        cuadroNegro.innerHTML = "Debe ingresar su correo";	 
+        cuadroNegro.className = "mostrar";   
+	}
+
+	//  Validar una dirección de email//
+	else if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(correo.value)) ) {
+        cuadroNegro.innerHTML = "Formato de correo incorrecto";	 
+        cuadroNegro.className = "mostrar";         
+	};
+};
+
+var validarTextarea = function(texto){
+	var cuadroNegro = document.getElementById("mensaje-error-texto");
+	// Validar un campo de texto obligattextoorio //
+	if (texto.value == null || texto.value == 0 || /^\s+$/.test(texto.value) ) {
+       cuadroNegro.innerHTML = "Debe ingresar mensaje";	
+       cuadroNegro.className = "mostrar";
+	};			 
+
+}
