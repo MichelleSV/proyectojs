346c35c647811ab2ee6d0d922d356297fc7b27e8
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-09-26 07:09:19 -0500

    agregando fondo al formulario y cambiando canciones del drag and drop

--- js/app.js
+++ js/app.js
@@ -2,52 +2,45 @@ window.onload = function () {
 
 	var frase =document.getElementById("frase");
 	frase.addEventListener("mouseover", function() {
-			frase.innerHTML =" diversión en los mejores eventos organizados por nosotros"
-	});		
+		frase.innerHTML =" diversión en los mejores eventos organizados por nosotros"
+	});
 	frase.addEventListener("mouseout", function() {
-			frase.innerHTML ="Entretenimiento en música electrónica.."
+		frase.innerHTML ="Entretenimiento en música electrónica.."
 	});	
 	var imagenes = document.getElementById("imagenes");
 	var bloque=document.getElementById("bloque");
 	var bloque2=document.getElementById("bloque2");
 	var bloque3=document.getElementById("bloque3");
 	var bloque4=document.getElementById("bloque4");
-		
 	imagenes.addEventListener("mouseover", function() {
-			bloque.classList.add("no");
-			bloque2.classList.remove("no"); 
-			bloque3.classList.add("no");
-			bloque4.classList.remove("no"); 
-
+		bloque.classList.add("no");
+		bloque2.classList.remove("no");
+		bloque3.classList.add("no");
+		bloque4.classList.remove("no");
 	});
 	imagenes.addEventListener("mouseout", function() {
-			bloque.classList.remove("no");
-			bloque2.classList.add("no"); 
-			bloque3.classList.remove("no");
-			bloque4.classList.add("no"); 
+		bloque.classList.remove("no");
+		bloque2.classList.add("no"); 
+		bloque3.classList.remove("no");
+		bloque4.classList.add("no"); 
 	});
-
 	imagenes.addEventListener("touchstar", function() {
-			bloque.classList.add("no");
-			bloque2.classList.remove("no"); 
-			bloque3.classList.add("no");
-			bloque4.classList.remove("no"); 
-
+		bloque.classList.add("no");
+		bloque2.classList.remove("no");
+		bloque3.classList.add("no");
+		bloque4.classList.remove("no");
 	});
 	imagenes.addEventListener("touchend", function() {
-			bloque.classList.remove("no");
-			bloque2.classList.add("no"); 
-			bloque3.classList.remove("no");
-			bloque4.classList.add("no"); 
+		bloque.classList.remove("no");
+		bloque2.classList.add("no"); 
+		bloque3.classList.remove("no");
+		bloque4.classList.add("no"); 
 	});
-
 	/*formulario*/
-		var btn = document.getElementById("boton");
+	var btn = document.getElementById("boton");
 	var nombre = document.getElementById("input-form1");
 	var correo = document.getElementById("input-form2");
 	var texto = document.getElementById("textarea-form");
-
-
 	btn.addEventListener("click", function(event){
 		event.preventDefault();
 		validarForm();
@@ -80,51 +73,43 @@ window.onload = function () {
 	texto.addEventListener("blur", function(){
 		validarTextarea(texto);
 	});		
-
-
 	function validarForm(){
 		validarNombre(nombre);
 		validarCorreo(correo);
 		validarTextarea(texto);
 	}
 };
-
-
 var validarNombre = function(nombre){
-    var cuadroNegro = document.getElementById("mensaje-error-nombre");
+	var cuadroNegro = document.getElementById("mensaje-error-nombre");
 	// Validar un campo de texto obligatorio //
 	if (nombre.value == null || nombre.value == 0 || /^\s+$/.test(nombre.value) ) {
-        cuadroNegro.innerHTML = "Debe ingresar su nombre";
-        cuadroNegro.className = "mostrar";
+		cuadroNegro.innerHTML = "Debe ingresar su nombre";
+		cuadroNegro.className = "mostrar";
 	}
-
 	// Validar un campo de texto nombre solo caracteres nombre//
 	else if ( /^[a-z][a-z]*/.test(nombre.value)==false ) {	
-        cuadroNegro.innerHTML = "Debe ingresar su caracteres correctos";  
-        cuadroNegro.className = "mostrar";   
+		cuadroNegro.innerHTML = "Debe ingresar su caracteres correctos";  
+		cuadroNegro.className = "mostrar";   
 	};
 };
-
 var validarCorreo = function(correo){
 	var cuadroNegro = document.getElementById("mensaje-error-correo");
 	// Validar un campo de texto obligatorio //
 	if (correo.value == null || correo.value == 0 || /^\s+$/.test(correo.value) ) {
-        cuadroNegro.innerHTML = "Debe ingresar su correo";	 
-        cuadroNegro.className = "mostrar";   
+		cuadroNegro.innerHTML = "Debe ingresar su correo";	 
+		cuadroNegro.className = "mostrar";   
 	}
-
 	//  Validar una dirección de email//
 	else if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(correo.value)) ) {
-        cuadroNegro.innerHTML = "Formato de correo incorrecto";	 
-        cuadroNegro.className = "mostrar";         
+		cuadroNegro.innerHTML = "Formato de correo incorrecto";	 
+		cuadroNegro.className = "mostrar";         
 	};
 };
-
 var validarTextarea = function(texto){
 	var cuadroNegro = document.getElementById("mensaje-error-texto");
 	// Validar un campo de texto obligattextoorio //
 	if (texto.value == null || texto.value == 0 || /^\s+$/.test(texto.value) ) {
-       cuadroNegro.innerHTML = "Debe ingresar mensaje";	
-       cuadroNegro.className = "mostrar";
+		cuadroNegro.innerHTML = "Debe ingresar mensaje";	
+		cuadroNegro.className = "mostrar";
 	};			 
 }
